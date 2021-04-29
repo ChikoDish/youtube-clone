9e6891b76fbea5370dd30f3a6e44271c712d02b2
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-04-29 08:27:13 +0530

    new player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -8,8 +8,17 @@ const VideoDetail = ({ video }) => {
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
   return (
     <div>
-      <div className="ui embed">
-        <iframe src={videoSrc} allowFullScreen title="Video player" />
+      <div className="ui embed video-responsive">
+        <iframe
+          width="853"
+          height="480"
+          src={videoSrc}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        />
+        {/* <object data={videoSrc} width="560px" height="315px"></object> */}
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
