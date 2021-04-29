6a04b3389c62ab88ce115ad84e03a98b8769d218
Ankit Kumar Gupta <ankitkumargupta00@gmail.com>
2021-04-29 08:38:48 +0530

    new Player

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const KEY = "";
+const KEY = "AIzaSyBLG7SCSGD0sfvbZ6rCFWVtjC6C1h75hO0";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
 const VideoDetail = ({ video }) => {
   if (!video) {
@@ -9,7 +10,7 @@ const VideoDetail = ({ video }) => {
   return (
     <div>
       <div className="ui embed video-responsive">
-        <iframe
+        {/* <iframe
           width="853"
           height="480"
           src={videoSrc}
@@ -17,8 +18,9 @@ const VideoDetail = ({ video }) => {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           title="Embedded youtube"
-        />
+        /> */}
         {/* <object data={videoSrc} width="560px" height="315px"></object> */}
+        <ReactPlayer controls={true} url={videoSrc} />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
